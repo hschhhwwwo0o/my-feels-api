8a826164491cb5c0b445326336c353a35367f30a
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-19 15:42:57 +0300

    Initializing a note model

--- src/models/notes.model.ts
+++ src/models/notes.model.ts
@@ -1,22 +1,23 @@
-// notes-model.ts - A mongoose model
-//
-// See http://mongoosejs.com/docs/models.html
-// for more of what you can do here.
-import { Application } from '../declarations';
-import { Model, Mongoose } from 'mongoose';
+import { Application } from "../declarations";
+import { Model, Mongoose } from "mongoose";
 
 export default function (app: Application): Model<any> {
-  const modelName = 'notes';
-  const mongooseClient: Mongoose = app.get('mongooseClient');
+  const modelName = "notes";
+  const mongooseClient: Mongoose = app.get("mongooseClient");
   const { Schema } = mongooseClient;
-  const schema = new Schema({
-    text: { type: String, required: true }
-  }, {
-    timestamps: true
-  });
+  const schema = new Schema(
+    {
+      title: { type: String, required: false },
+      text: { type: String, required: false },
+      authorID: { type: String, required: true },
+      emojies: { type: Array, required: false },
+      isPinned: { type: Boolean, required: false },
+    },
+    {
+      timestamps: true,
+    }
+  );
 
-  // This is necessary to avoid model compilation errors in watch mode
-  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
   if (mongooseClient.modelNames().includes(modelName)) {
     (mongooseClient as any).deleteModel(modelName);
   }
