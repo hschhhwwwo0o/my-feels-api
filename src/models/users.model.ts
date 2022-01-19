998eddb876771f8f55de3078817cd3e31530dfe7
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-19 15:26:10 +0300

    Primary description of the user model

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -4,10 +4,14 @@ import { Model, Mongoose } from "mongoose";
 export default function (app: Application): Model<any> {
   const modelName = "users";
   const mongooseClient: Mongoose = app.get("mongooseClient");
+
   const schema = new mongooseClient.Schema(
     {
-      email: { type: String, unique: true, lowercase: true },
+      email: { type: String, unique: true },
       password: { type: String },
+      firstName: { type: String, unique: false },
+      lastName: { type: String, unique: false },
+      theme: { type: String, unique: false, lowercase: true },
     },
     {
       timestamps: true,
@@ -17,5 +21,6 @@ export default function (app: Application): Model<any> {
   if (mongooseClient.modelNames().includes(modelName)) {
     (mongooseClient as any).deleteModel(modelName);
   }
+
   return mongooseClient.model<any>(modelName, schema);
 }
