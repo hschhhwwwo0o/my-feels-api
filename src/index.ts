76d9d615b5e6ae57862d5b85f68f547e85a14cb3
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-19 18:59:14 +0300

    Refactoring code

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const port = process.env.PORT || app.get("port");
 const host = process.env.HOST || app.get("host");
+
 const server = app.listen(port);
 
 process.on("unhandledRejection", (reason, p) =>
