60b25e7fb4b828e74406b65933e43a2297bd3e88
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-21 20:51:18 +0300

    Add types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+import { Server } from "http";
 import logger from "./logger";
 import app from "./app";
 
 require("dotenv").config();
 
-const port = process.env.PORT || app.get("port");
-const host = process.env.HOST || app.get("host");
+const port: string = process.env.PORT || app.get("port");
+const host: string = process.env.HOST || app.get("host");
 
-const server = app.listen(port);
+const server: Server = app.listen(port);
 
 process.on("unhandledRejection", (reason, p) =>
   logger.error("Unhandled Rejection at: Promise ", p, reason)
